6a1ce1cdb9e954ddea3a474d4536238f8484ee97
Larin-ai <larin_ai00@mail.ru>
2020-07-13 21:37:53 +0300

    Сложение двух чисел( как вводить символы сложения пока не понял)

---
+++ src/Task2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Task2 {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, c;
+
+        System.out.println("Введите первое число");
+        a = sc.nextInt();
+
+        System.out.println("Введите второе число");
+        b = sc.nextInt();
+
+        c = a + b;
+
+        System.out.println("c = " + c);
+    }
+}
